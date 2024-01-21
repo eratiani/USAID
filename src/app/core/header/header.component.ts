621929f6844c2a07b8d961095b2fa38e96ed2458
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2024-01-22 00:25:02 +0400

    finish header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ResizeListenerService } from '../services/resize-listener.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  widowPosY!: number;
+  windowWidth!: number;
+  windowPosYSub!: Subscription;
+  windowWidthSub!: Subscription;
+  constructor(private scrollServ: ResizeListenerService) {}
 
+  ngOnInit(): void {
+    this.widowPosY = this.scrollServ.getScrollPosition();
+    this.windowWidth = this.scrollServ.getScreenWidth();
+    this.windowPosYSub = this.scrollServ.scrollPosition.subscribe(
+      (val) => (this.widowPosY = val)
+    );
+    this.windowWidthSub = this.scrollServ.screenWidth.subscribe(
+      (val) => (this.widowPosY = val)
+    );
+  }
+  ngOnDestroy(): void {
+    this.windowPosYSub.unsubscribe();
+    this.windowWidthSub.unsubscribe();
+  }
 }
